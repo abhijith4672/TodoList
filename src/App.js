2ed2e4215c4ddcc1108e76d307e51267031ce636
abhijith4672 <91078870+abhijith4672@users.noreply.github.com>
2022-07-24 20:15:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+import { useState } from "react";
+export default function App() {
+  const [todos, setTodos] = useState([{ text: "Apple", isCompleted: false }]);
+  const [value, setValues] = useState("");
+  function addItem(e) {
+    e.preventDefault();
+    if (!value) return;
+    const newtodos = [...todos, { text: value, isCompleted: false }];
+    setTodos(newtodos);
+    setValues("");
+  }
+  function removeItem(e) {
+    var index = Number(e.target.id);
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+  return (
+    <>
+      <form onSubmit={addItem}>
+        <input
+          type="text"
+          className="input"
+          placeholder="Add item"
+          value={value}
+          onChange={(e) => setValues(e.target.value)}
+        />
+      </form>
+      {todos.map((item, i) => (
+        <div className="todo" key={i} id={i}>
+          {item.text}
+          <button key={i} id={i} onClick={removeItem}>
+            X
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
